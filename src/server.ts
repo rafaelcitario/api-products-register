4f7180f2f53c3300c0b1e8f676bf41d9fef91d72
Rafael <contato.rafaelgomes@outlook.com>
2024-03-24 02:06:18 -0300

    add isSale at table products

--- src/server.ts
+++ src/server.ts
@@ -1,30 +1,19 @@
-import fastify, { RequestBodyDefault } from 'fastify'
+import fastify from 'fastify'
 import { env } from './env'
 import { knex } from './database'
 const app = fastify()
 
 app.post('/product', async (request, response) => {
-  const { name, price } = request.body
+  const { name, price, isSale } = request.body
+  const product = {}
 
-  const products = await knex('products')
-    .insert({
-      id: crypto.randomUUID(),
-      name,
-      price,
-    })
-    .returning('*')
+  const products = await knex('products').insert({ product }).returning('*')
   return response.send(products)
-  // ------------------------------------------
-  // products.uuid('id').primary().index()
-  // products.text('name').notNullable()
-  // products.decimal('price', 10, 2).notNullable()
-  // products.timestamp('created_at').defaultTo(knex.fn.now())
 })
 
-app.get('/list', async () => {
-  return await knex('products').select('*')
+app.get('/list', async (request, response) => {
+  return response.send(await knex('products').select('*'))
 })
-
 app.listen({ port: env.PORT, host: env.HOST }, () =>
   console.log('server is up!'),
 )
