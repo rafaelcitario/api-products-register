28ab47ec7959aa62998e232391e60809bfe54469
Rafael <contato.rafaelgomes@outlook.com>
2024-03-24 03:03:19 -0300

    separate function sort

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,26 @@
 import fastify from 'fastify'
 import { env } from './env'
 import { knex } from './database'
+import { randomUUID } from 'crypto'
+import { orderProductsByPrice } from '../service/orderedListProducts'
 const app = fastify()
 
 app.post('/product', async (request, response) => {
-  const { name, price, isSale } = request.body
-  const product = {}
-
-  const products = await knex('products').insert({ product }).returning('*')
-  return response.send(products)
+  await knex('products')
+    .insert({
+      id: randomUUID(),
+      name: request.body.name,
+      price: request.body.price,
+      isSale: request.body.isSale,
+    })
+    .returning('*')
+  return response.send(await orderProductsByPrice())
 })
 
 app.get('/list', async (request, response) => {
-  return response.send(await knex('products').select('*'))
+  return response.send(await orderProductsByPrice())
 })
+
 app.listen({ port: env.PORT, host: env.HOST }, () =>
   console.log('server is up!'),
 )
